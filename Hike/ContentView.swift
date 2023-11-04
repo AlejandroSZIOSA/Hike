a083750ac259475ce2a620d3bc044ca7e1cdef5e
AlejandroSZIOSA <ggato33@gmail.com>
2023-11-04 16:40:12 +0100

    Initial Commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by gabriel Sazo on 2023-11-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by gabriel Sazo on 2023-11-04.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
