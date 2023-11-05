9a306cb5ad654a5796c4e4fa000d82db1878a473
AlejandroSZIOSA <ggato33@gmail.com>
2023-11-05 13:01:23 +0100

    Header and sub-header

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -9,32 +9,54 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-      
       //-CARD
-      
       ZStack {
-        
         CostomBackgroundView()
-        
-        ZStack{
-              Circle()
-                  .fill(
-                      LinearGradient(
-                          colors:[
-                            Color("ColorIndigoMedium"),
-                            Color("ColorSlmonLight")
-                          ],
-                          startPoint: .topLeading,
-                          endPoint: .bottomTrailing
-                      )
-                  )
-                  .frame(width: 256, height: 256)
+        VStack {
+          // .HEADER
+          VStack(alignment: .leading) {
+            HStack {
+              Text ("Hiking")
+                .fontWeight(.black)
+                .font(.system(size: 52))
+                .foregroundStyle(
+                  LinearGradient(
+                  colors:[
+                  .customGrayLight,
+                  .customGrayMedium],
+                  startPoint:.top,
+                  endPoint:.bottom)
+              )
+            }
+            Text("Fun and enjoyable outdoor activity for friends and families.")
+              .multilineTextAlignment(.leading)
+              .foregroundColor(.customGrayMedium)
+            
+            
+          }//HEADER
+          .padding(.horizontal, 30)
+          //MAIN CONTENT
           
+          ZStack{
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors:[
+                          Color("ColorIndigoMedium"),
+                          Color("ColorSlmonLight")
+                        ],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+                .frame(width: 256, height: 256)
+            Image("image-1")
+                .resizable()
+                .scaledToFit()
+          }
+          //FOOTER
           
-          Image("image-1")
-              .resizable()
-              .scaledToFit()
-        }
+        }//VSTACK
       } //CARD
       .frame(width: 320, height: 570)
     }
